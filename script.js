bcec7932eb23257eafc40ad4f96b2c4e41a91dd0
mansooriabbas <mansoori.abbas@outlook.com>
2024-01-24 16:09:56 +0100

    completed 1 - 7

---
+++ script.js
@@ -0,0 +1,32 @@
+const recipeName = document.querySelector("#recipe-name").innerText;
+const recipeTagName = document.querySelector("#recipe-name").tagName;
+const description = document.querySelector(".description");
+const fontSize = window.getComputedStyle(description);
+const imageAltValue = document.querySelector("img").getAttribute("alt");
+const image = document.querySelector("img");
+const imgComputedStyle = window.getComputedStyle(image);
+const paste = document.querySelector(".ingredients-list-paste").children.length;
+const forthIngredient = document.querySelector(".ingredients-list-paste")
+  .children[3].innerText;
+const instructions = document.querySelectorAll(".instructions-list");
+
+const dimensionOfImg = {
+  url: image.getAttribute("src"),
+  width: imgComputedStyle.width,
+  height: imgComputedStyle.height,
+};
+
+const instructionsArray = [];
+
+
+
+console.log(instructions);
+
+console.log(instructionsArray);
+console.log(paste);
+console.log(forthIngredient);
+console.log(dimensionOfImg);
+console.log(imageAltValue);
+console.log(fontSize.fontSize);
+console.log(recipeName);
+console.log(recipeTagName);
